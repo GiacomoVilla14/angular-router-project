2288deb4290a451c617b70958865e993132355fa
GiacomoVilla14 <g.villa@itsrizzoli.it>
2023-07-10 17:29:48 +0200

    ngOnInit ngOnDestroy

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, OnDestroy {
+  private intervalSubscription?: Subscription;
 
+  constructor() {}
+
+  ngOnInit() {
+    this.intervalSubscription = interval(1000).subscribe((count) => {
+      console.log(count);
+    });
+  }
+  ngOnDestroy() {
+    this.intervalSubscription?.unsubscribe();
+  }
 }
