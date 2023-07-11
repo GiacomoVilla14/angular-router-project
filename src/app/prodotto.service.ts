55b6b90731953fa8e1fa4accd6738b9cd643a831
GiacomoVilla14 <g.villa@itsrizzoli.it>
2023-07-11 15:07:28 +0200

    Aggiunta nuovo Observable

--- src/app/prodotto.service.ts
+++ src/app/prodotto.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdottoService {
+  public observable?: Observable<number>
 
-  constructor() { }
+  constructor() {
+    this.observable = new Observable<number>(subscriber => {
+      let count = 0
+      setInterval(() => {
+        subscriber.next(count)
+        count++
+      }, 1000)
+    })
+  }
 }
