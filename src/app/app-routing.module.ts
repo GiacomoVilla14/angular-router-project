ceb9be761e106d18e194560d1f94c84d616edb00
GiacomoVilla14 <g.villa@itsrizzoli.it>
2023-07-11 14:48:00 +0200

    Aggiunto myCanActivate all app-routing.module.ts togliendo AuthGuardService

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ProductsComponent } from './products/products.component';
-import { ProductComponent } from './product/product.component';
-import { LoginComponent } from './login/login.component';
-import { CheckoutComponent } from './checkout/checkout.component';
-import { ProfileComponent } from './profile/profile.component';
-import { AuthGuardService } from './auth-guard.service';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ProductsComponent} from './products/products.component';
+import {ProductComponent} from './product/product.component';
+import {LoginComponent} from './login/login.component';
+import {CheckoutComponent} from './checkout/checkout.component';
+import {ProfileComponent} from './profile/profile.component';
+import {myCanActivate} from './auth-guard.service';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'products', component: ProductsComponent },
-  { path: 'product', component: ProductComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'checkout', component: CheckoutComponent },
+  {path: '', component: HomeComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'checkout', component: CheckoutComponent},
   {
     path: 'profile',
     component: ProfileComponent,
-    canActivate: [AuthGuardService],
+    canActivate: [myCanActivate],
   },
 ];
 
@@ -25,4 +25,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
